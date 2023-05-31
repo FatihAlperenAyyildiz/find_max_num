cc3e8a19299259b31689ac4a62c001a2283fcff1
FatihAlperenAyyildiz <135124178+FatihAlperenAyyildiz@users.noreply.github.com>
2023-05-31 14:52:57 +0300

    Update find_max.py

--- find_max.py
+++ find_max.py
@@ -11,8 +11,13 @@ except:
 def en_büyüğünü_bul(sayılar):
     en_büyük_sayı = float('-inf')
     for sayı in sayılar:
+        print('sayı: ' + str(sayı))
         if sayı > en_büyük_sayı:
+            print('eğer sayı: ' + str(sayı) + ', büyükse, en_büyük_sayı: ' + str(en_büyük_sayı) + ' dan')
             en_büyük_sayı = sayı
+            print('en_büyük_sayı: ' + str(en_büyük_sayı))
+        else:
+            print('eğer sayı: ' + str(sayı) + ', küçükse, en_büyük_sayı: ' + str(en_büyük_sayı) + ' dan, o zaman bir şey yapmadan devam ediyoruz')
     return en_büyük_sayı
 
 sonuç = en_büyüğünü_bul(sayılarım)
